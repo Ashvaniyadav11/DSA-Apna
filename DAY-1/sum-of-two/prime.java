d7922d0f8d6ff8d5130768b7e6a49b30d687666b
Ashvaniyadav11 <ashvaniyadav21437@gmail.com>
2025-11-11 01:04:50 +0530

    Prime is completed but need extra more one time

--- DAY-1/sum-of-two/prime.java
+++ DAY-1/sum-of-two/prime.java
@@ -1,16 +1,30 @@
 import java.util.Scanner;
-public class prime{
-    public static void isprime(int n){
-        if(n%2==0 || n%1==1){
-            System.out.println(n+ " is not prime ");
+
+public class prime {
+
+    // Method to check if a number is prime
+    public static boolean isPrime(int n) {
+        if (n < 2) return false; // 0 and 1 are not prime
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false; // found a divisor
+            }
         }
-        else{
-            System.out.println(n+" is  prime");
-        }
+        return true; // no divisors found
     }
-    public static void main(String args[]){
+
+    public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
-        int nu = scn.nextInt();
-        isprime(nu);
+
+        System.out.print("Enter a number: ");
+        int num = scn.nextInt();
+
+        if (isPrime(num)) {
+            System.out.println(num + " is a prime number.");
+        } else {
+            System.out.println(num + " is not a prime number.");
+        }
+
+        
     }
 }
